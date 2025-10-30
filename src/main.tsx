279e394299db8957c7b10000c933e64309abd519
Alejandro Lopez <alopezbelles@gmail.com>
2025-10-30 16:53:24 +0100

    navbar desgin

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.css";
+// import App from "./App.tsx";
+import { BrowserRouter, Routes } from "react-router";
+import Header from "./components/Header/Header.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      {/* Aquí irán tus rutas */}
+    </Routes>
+    {/* Puedes agregar <App /> aquí si necesitas contenido adicional */}
+  </BrowserRouter>
+);
